fb08eac64365358a53be9e0ad13e23aaeaccd80f
FruitCakeS <sean_lu@live.com>
2017-08-15 17:07:52 -0400

    added fetch retries

--- FetchPrices.py
+++ FetchPrices.py
@@ -8,7 +8,13 @@ reader = open(b"mtg_sets.obj","rb")
 
 mtg_sets = pickle.load(reader)
 
-sets_to_fetch = [] 
+try:
+    reader = open(b"mtg_sets_count.meta","rb")
+    mtg_sets_count = pickle.load(reader)
+except:
+    mtg_sets_count = {}
+
+sets_to_fetch = []
 for s in mtg_sets:
     if s.type in ["expansion", "core", "masters", "reprint", "from the vault", "starters", "premium deck", "duel deck", "commander", "planechase", "archenemy", "conspiracy", "masterpiece"]:
         sets_to_fetch.append(s.code)
@@ -21,27 +27,38 @@ prices = {"time":current_time}
 
 outfile = open("raw_price_list/"+current_time+".txt", "w")
 
+set_count = 0
 for s in sets_to_fetch:
-    print "fetching prices for set " + s + "..."
-    set_prices = fetch_prices(s, True, False)
-    prices[s+"TrueFalse"] = set_prices
-    for card in set_prices:
-        outfile.write(card[0]+ " "+s+" online nonfoil "+str(card[1])+"\n")
-    
-    set_prices = fetch_prices(s, True, True)
-    prices[s+"TrueTrue"] = set_prices
-    for card in set_prices:
-        outfile.write(card[0]+ " "+s+" online foil "+str(card[1])+"\n")
+    set_count += 1
+    print "fetching prices for set " + s + "... (set "+str(set_count)+" of "+str(len(sets_to_fetch))+")"
+    for (k, online, foil) in [
+        (s+" online nonfoil", True, False),
+        (s+" online foil", True, True),
+        (s+" paper nonfoil", False, False),
+        (s+" paper foil", False, True)]:
 
-    set_prices = fetch_prices(s, False, False)
-    prices[s+"FalseFalse"] = set_prices
-    for card in set_prices:
-        outfile.write(card[0]+ " "+s+" paper nonfoil "+str(card[1])+"\n")
+        expected_count = 0
+        if k in mtg_sets_count.keys():
+            expected_count = mtg_sets_count[k]
+        
+        retries = 3
+        set_prices = []
+        actual_count = 0
+        while retries>0:
+            retries -= 1
+            set_prices = fetch_prices(s, online, foil)
+            actual_count = len(set_prices)
+            if (actual_count >= expected_count):
+                break
 
-    set_prices = fetch_prices(s, False, True)
-    prices[s+"FalseTrue"] = set_prices
-    for card in set_prices:
-        outfile.write(card[0]+ " "+s+" paper foil "+str(card[1])+"\n")
+        prices[k] = set_prices
+        for card in set_prices:
+            outfile.write(card[0]+ " "+k+" "+str(card[1])+"\n")
+
+        
+        print k+" expected "+str(expected_count)+" cards got "+str(actual_count)+" cards"
+
+        mtg_sets_count[k] = max(expected_count,actual_count)
 
     print "done fetching set "+s
 
@@ -50,4 +67,9 @@ for s in sets_to_fetch:
 print "pickling prices..."
 filehandler = open(b"raw_price_list/"+current_time+".obj","wb")
 pickle.dump(prices,filehandler)
+print "done"
+
+print "pickling counts..."
+filehandler = open(b"mtg_sets_count.meta","wb")
+pickle.dump(mtg_sets_count,filehandler)
 print "done"
